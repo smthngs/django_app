b7a73e448ea873a6902cbffe83b24f65ea6b0e9d
smthngs <119532776+smthngs@users.noreply.github.com>
2022-12-25 19:27:26 +0300

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Uncomment if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
